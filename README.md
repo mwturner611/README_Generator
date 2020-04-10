53b4a4a79414bac949ed72b44ee0816c510a7e05
mwturner611 <mwturner611@gmail.com>
2020-04-09 23:32:22 -0500

    Consolidate to one JS file

--- README.md
+++ README.md
@@ -1,67 +1,56 @@
-###### Badges
-
-<a href="https://mwturner611.github.io"><img alt="Portfolio" src="https://img.shields.io/badge/-Portfolio-green"></a>
-
-<a href="https://www.linkedin.com/in/matt-turner-ba328211a"><img alt="LinkedIn" src="https://img.shields.io/badge/-LinkedIn-blue"></a>
-
-
-# README_Generator
-###### Description
-<p>Using Node.js this app creates clean, professional README's easily and quickly from user inputs in the command line.  This app exists to save time for the developer to focus more attention on the application.</p>
-
-# Generating a README.md
-<img src="images/demo.gif">
-
-
-## Table of Contents (Optional)
-* [Top of Page](#badges)
-* [Gif](#description)
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Contacts](#contacts)
-
-## Installation
-1. Clone or Download
-2. This app requires you link to a personal access token to your github allowing access to your user email address [TokenInstructions](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line)
-3. On navigation to correct file install packages npm i
-4. Done. Time to use it.
-
-## Usage 
-1. From command line type node index.js
-2. Follow the prompts as requested
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-
-## License
-This project is licensed under The MIT License (MIT).
-
-## Contributing
-All contributions and suggestions are welcome! For direct contributions, please fork the repository and file a pull request.
-
-## Tests
-1. Validate that you have installed are the correct packages:
-* inquirer
-2. Validate that you have linked correctly to your .env file containing your github token
-* [GitHub_Token](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line)
-* [dotenv](https://www.npmjs.com/package/dotenv)
-
-## Contacts
-<img src="images/profile.jfif">
-
-* Name: Matt Turner (@mwturner611)
-* e-mail: mwturner611@gmail.com
-* [LinkedIn](https://www.linkedin.com/in/matt-turner-ba328211a/)
-
-
+  
+    <img alt="License" src="https://img.shields.io/badge/-MIT License-blue">
+        
+    # README_Generator
+    ###### Description
+    <p>Using Node.js this app creates clean, professional README's easily and quickly from user inputs in the command line. This app exists to save time for the developer to focus more attention on the application.</p>
+    
+    # Generating a README.md
+    <img src="images/demo.gif">
+    
+    
+    ## Table of Contents (Optional)
+    * [Top of Page](#badges)
+    * [Gif](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Contacts](#contacts)
+    
+    ## Installation
+    1. Clone or Download
+    2. This app requires you link to a personal access token to your github allowing access to your user email address
+    3. On navigation to correct file install packages npm i
+    4. Done. Time to use it.
+    undefined
+    
+    ## Usage 
+    1. From command line type node index.js
+    2. Follow the prompts as requested
+    undefined
+    undefined
+    undefined
+    
+    ## Credits
+    TerrenceMM2 helped with sequencing the code to work with the axios call.    
+    
+    
+    
+    ## License
+    This project is licensed under The MIT License.
+    
+    ## Contributing
+    All contributions and suggestions are welcome! For direct contributions, please fork the repository and file a pull request.
+    
+    ## Tests
+    
+    
+    ## Contacts
+    <img src="https://avatars0.githubusercontent.com/u/58919488?v=4">
+    
+    * Name: Matt Turner (mwturner611)
+    * e-mail: mwturner611@gmail.com
+    * [LinkedIn](https://www.linkedin.com/in/matt-turner-ba328211a/)
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// bring in inquirer
+// bring in inquirer,fs,util
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const env = require("dotenv").config({path: '.env'});
+const axios = require('axios');
 
-// look up util.promisify to understand
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
+// Define user prompt function
 function promptUser() {
     return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },      
       {
         type: "input",
         name: "username",
@@ -16,41 +20,219 @@ function promptUser() {
       },
       {
         type: "input",
-        name: "location",
-        message: "Where are you from?"
+        name: "title",
+        message: "Title: What is the title of your project?"
       },
       {
-        type: "input",
-        name: "hobby",
-        message: "What is your favorite hobby?"
-      },
-      {
-        type: "input",
-        name: "food",
-        message: "What is your favorite food?"
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Enter your GitHub Username"
+        type: "list",
+        name: "license",
+        message: "License: What license is this project?",
+        choices: [
+          'MIT License',
+          'GNU GPLv3',
+          'GNU AGPLv3',
+          'Mozilla Public License 2.0'
+        ]
       },
       {
         type: "input",
         name: "linkedin",
-        message: "Enter your LinkedIn URL."
-      }
+        message: "LinkedIn: What's your linked in URL?"
+      },
+
+      {
+        type: "input",
+        name: "description",
+        message: "Description: Please provide a description of your project"
+      },
+      {
+        type: "list",
+        name: "linesInstall",
+        message: "Install: Please select the number of lines needed for install instructions",
+        choices: [1,2,3,4,5]
+      },
+      {
+        type: "input",
+        name: "instal1",
+        message: "This is your first line for the install. Start with '1. '"
+      },
+      {
+        type: "input",
+        name: "instal2",
+        message: "This is your second line for the install. Start with '2. '",
+        when: (answers) => answers.linesInstall > 1
+      },
+      {
+        type: "input",
+        name: "instal3",
+        message: "This is your third line for the install. Start with '3. '",
+        when: (answers) => answers.linesInstall > 2
+      },
+      {
+        type: "input",
+        name: "instal4",
+        message: "This is your fourth line for the install. Start with '4. '",
+        when: (answers) => answers.linesInstall > 3
+      },
+      {
+        type: "input",
+        name: "instal5",
+        message: "This is your fifth line for the install. Start with '5. '",
+        when: (answers) => answers.linesInstall > 4
+      },
+      {
+        type: "list",
+        name: "linesUsage",
+        message: "Usage: Please select the number of lines needed for usage instructions",
+        choices: [1,2,3,4,5]
+      },
+      {
+        type: "input",
+        name: "usage1",
+        message: "This is your first line for the usage. Start with '1. '"
+      },
+      {
+        type: "input",
+        name: "usage2",
+        message: "This is your second line for the usage. Start with '2. '",
+        when: (answers) => answers.linesUsage > 1
+      },
+      {
+        type: "input",
+        name: "usage3",
+        message: "This is your third line for the usage. Start with '3. '",
+        when: (answers) => answers.linesUsage > 2
+      },
+      {
+        type: "input",
+        name: "usage4",
+        message: "This is your fourth line for the usage. Start with '4. '",
+        when: (answers) => answers.linesUsage > 3
+      },
+      {
+        type: "input",
+        name: "usage5",
+        message: "This is your fifth line for the usage. Start with '5. '",
+        when: (answers) => answers.linesUsage > 4
+      },
+      {
+        type: "input",
+        name: "credits",
+        message: "Credits: please list anyone or materials that made this possible"
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Contributing: Please leave instructions for those who want to contribute"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Tests: Please describe any tests for this code"
+      },
     ]);
   }
 
+// create the function that generates the readme
+  function generateMarkdown(data,data2) {
+    const readme = `  
+    <img alt="License" src="https://img.shields.io/badge/-${data.license}-blue">
+        
+    # ${data.title}
+    ###### Description
+    <p>${data.description}</p>
+    
+    # Generating a README.md
+    <img src="images/demo.gif">
+    
+    
+    ## Table of Contents (Optional)
+    * [Top of Page](#badges)
+    * [Gif](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Contacts](#contacts)
+    
+    ## Installation
+    ${data.instal1}
+    ${data.instal2}
+    ${data.instal3}
+    ${data.instal4}
+    ${data.instal5}
+    
+    ## Usage 
+    ${data.usage1}
+    ${data.usage2}
+    ${data.usage3}
+    ${data.usage4}
+    ${data.usage5}
+    
+    ## Credits
+    ${data.credits}    
+    
+    
+    
+    ## License
+    This project is licensed under The ${data.license}.
+    
+    ## Contributing
+    ${data.contributing}
+    
+    ## Tests
+    ${data.tests}
+    
+    ## Contacts
+    <img src="${data2.avatarURL}">
+    
+    * Name: ${data.name} (${data.username})
+    * e-mail: ${data2.email}
+    * [LinkedIn](${data.linkedin})`;
+    
+    fs.writeFile("README.md",readme, function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("Generated Readme!")
+      }
+    });
+}
+
+// call promptuser function
 promptUser()
   .then(function(answers) {
-    const readme = generateREADME(answers);
-
-    return writeFileAsync("README.md", readme);
+    // call axios call function
+    api.getUser(answers);
   })
-  .then(function() {
-    console.log("Successfully wrote to README.md");
-  })
-  .catch(function(err) {
+   .catch(function(err) {
     console.log(err);
   });
+
+// Define getUser function for axios call to github API
+const api = {
+  getUser(inquirer){
+                    axios
+                    .get(`https://api.github.com/users/${inquirer.username}`, {
+                      headers: {"Authorization": `token ${process.env.GH_TOKEN}`}})
+                    .then(response => {
+                      const githubInfo = {
+                        avatarURL: response.data.avatar_url,
+                        githubURL: response.data.url,
+                        email: response.data.email,
+                      }
+                       
+                      // send user responses and gethub info to readme generator
+                      const readme = generateMarkdown(inquirer,githubInfo);                      
+                    })   
+                    .catch(error => console.log(error))
+  }
+};
+
+  
+  
+
+ 
